b5e9fd4605ddcf1f87279c5493d9b9394a31b316
Chad Smith <chadsmithmusic@icloud.com>
2021-12-11 12:23:12 -0800

    part 2

--- part-2/app.js
+++ part-2/app.js
@@ -1,51 +1,54 @@
-// Part 1 Solution:
+$(function () {
+  let baseURL = "https://deckofcardsapi.com/api/deck";
 
-let number = 10;
-let favNumber = 10;
-let baseURL = "http://numbersapi.com";
-
-// 1.
-$.getJSON(`${baseURL}/${favNumber}?json`).then(data => {
-  console.log(data);
-});
-
-// 2.
-let favNumbers = [7, 11, 22];
-$.getJSON(`${baseURL}/${favNumbers}?json`).then(data => {
-  console.log(data);
-});
-
-// 3.
-Promise.all(
-  Array.from({ length: 4 }, () => {
-    return $.getJSON(`${baseURL}/${favNumber}?json`);
-  })
-).then(facts => {
-  facts.forEach(data => $("body").append(`<p>${data.text}</p>`));
-});
-
-// My attempt:
-
-axios
-  .get(`${baseURL}/${number}?json`)
-  .then((p1) => {
-    console.log(`${p1.data.text}`);
-    return axios.get(`${baseURL}/${number}/?json`);
-  })
-  .then((p2) => {
-    console.log(`${p2.data.text}`);
-    return axios.get(`${baseURL}/${number}/?json`);
-  })
-  .then((p3) => {
-    console.log(`${p3.data.text}`);
-    return axios.get(`${baseURL}/${number}/?json`);
-  })
-  .then((p4) => {
-    console.log(`${p4.data.text}`)
-    return axios.get(`${baseURL}/${number}/?json`);
-
-  })
-  .catch((err) => {
-    console.log(`Error :( ${err}`);
+  // 1.
+  $.getJSON(`${baseURL}/new/draw/`).then((data) => {
+    let { suit, value } = data.cards[0];
+    console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
   });
 
+  // 2.
+  let firstCard = null;
+  $.getJSON(`${baseURL}/new/draw/`)
+    .then((data) => {
+      firstCard = data.cards[0];
+      let deckId = data.deck_id;
+      return $.getJSON(`${baseURL}/${deckId}/draw/`);
+    })
+    .then((data) => {
+      let secondCard = data.cards[0];
+      [firstCard, secondCard].forEach(function (card) {
+        console.log(
+          `${card.value.toLowerCase()} of ${card.suit.toLowerCase()}`
+        );
+      });
+    });
+
+  // 3.
+  let deckId = null;
+  let $btn = $("button");
+  let $cardArea = $("#card-area");
+
+  $.getJSON(`${baseURL}/new/shuffle/`).then((data) => {
+    deckId = data.deck_id;
+    $btn.show();
+  });
+
+  $btn.on("click", function () {
+    $.getJSON(`${baseURL}/${deckId}/draw/`).then((data) => {
+      let cardSrc = data.cards[0].image;
+      let angle = Math.random() * 90 - 45;
+      let randomX = Math.random() * 40 - 20;
+      let randomY = Math.random() * 40 - 20;
+      $cardArea.append(
+        $("<img>", {
+          src: cardSrc,
+          css: {
+            transform: `translate(${randomX}px, ${randomY}px) rotate(${angle}deg)`,
+          },
+        })
+      );
+      if (data.remaining === 0) $btn.remove();
+    });
+  });
+});
